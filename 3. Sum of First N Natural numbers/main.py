540a4af48bd9d25ad813efc1bfd23be9094f53fa
dhruvdankhara <dhruvdankhara09@gmail.com>
2024-01-25 19:05:35 +0530

    add number 3 file

---
+++ 3. Sum of First N Natural numbers/main.py
@@ -0,0 +1,5 @@
+n = 6
+sum = 0
+for i in range(n + 1):
+    sum = sum + i
+print(f"sum of first {n} numbers is {sum}.")
---
+++ 3. Sum of First N Natural numbers/script.js
@@ -0,0 +1,7 @@
+let n = 6;
+let sum = 0;
+for(let i = 1; i <= n; i++){
+    sum += i;
+}
+
+console.log(`sum of first ${n} numbers is ${sum}.`);
