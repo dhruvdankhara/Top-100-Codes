da39335ac5fd8313a90f47f48334449170ac4c78
dhruvdankhara <dhruvdankhara09@gmail.com>
2024-01-25 19:19:44 +0530

    add number 3 file

--- 3. Sum of First N Natural numbers/main.py
+++ 3. Sum of First N Natural numbers/main.py
@@ -1,5 +1,14 @@
+# n = 6
+# sum = 0
+# for i in range(n + 1):
+#     sum += i
+# print(f"sum of first {n} numbers is {sum}.")
+
+
+# Using Recursion
 n = 6
-sum = 0
-for i in range(n + 1):
-    sum = sum + i
-print(f"sum of first {n} numbers is {sum}.")
+def sumOfN(num):
+    if num == 1:
+        return 1
+    return num + sumOfN(num - 1)
+print(f"sum of first {n} numbers is {sumOfN(n)}.")
--- 3. Sum of First N Natural numbers/script.js
+++ 3. Sum of First N Natural numbers/script.js
@@ -1,7 +1,17 @@
-let n = 6;
-let sum = 0;
-for(let i = 1; i <= n; i++){
-    sum += i;
+// let n = 6;
+// let sum = 0;
+// for(let i = 1; i <= n; i++){
+//     sum += i;
+// }
+// console.log(`sum of first ${n} numbers is ${sum}.`);
+
+// Using Recursion
+let n = 5;
+function sumOfN(num){
+    if(num === 1){
+        return 1;
+    }
+    return (num + sumOfN(num - 1));
 }
 
-console.log(`sum of first ${n} numbers is ${sum}.`);
+console.log(`sum of first ${n} numbers is ${sumOfN(n)}.`)
