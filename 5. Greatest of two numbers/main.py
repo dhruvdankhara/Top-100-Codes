ea113214b9fe299316466ae6e73bc398bae2ce76
dhruvdankhara <dhruvdankhara09@gmail.com>
2024-01-26 16:15:43 +0530

    add numbr 5 file

---
+++ 5. Greatest of two numbers/main.py
@@ -0,0 +1,8 @@
+a = 50
+b = 8
+if a == b:
+    print("both are equal.")
+elif a > b:
+    print(a, "is greatest.")
+else:
+    print(b,"is greatest.")
---
+++ 5. Greatest of two numbers/script.js
@@ -0,0 +1,10 @@
+let a = 50;
+let b = 7;
+
+if(a == b){
+    console.log("both are equal.");
+}else if(a > b){
+    console.log(a,"is greatest.");
+}else{
+    console.log(b,"is greatest.");
+}
